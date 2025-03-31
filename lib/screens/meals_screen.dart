d4ef98fafa9ec1799692bdf63cc209f9180abdd7
Minh Truong <truongcongminh96@gmail.com>
2025-03-31 13:55:03 +0700

    Introducing the Stack Widget

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/Models/meals.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -31,6 +32,14 @@ class MealsScreen extends StatelessWidget {
         ),
       );
     }
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) => MealItem(meal: meals[index]),
+      );
+    }
+
     return Scaffold(appBar: AppBar(title: Text(title)), body: content);
   }
 }
